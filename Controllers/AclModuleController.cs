c84d74c2e30dec7887f74d982fb0407986bded5e
siddique.porosh@softrobotics.com.bd <siddique.porosh@softrobotics.com.bd>
2024-03-29 16:47:33 +0600

    Init first commit

---
+++ Controllers/AclModuleController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+
+namespace ACL.Controllers
+{
+    public class AclModuleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Ok();
+        } 
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }  
+        public IActionResult View(int Id)
+        {
+            return View();
+        }  
+        public IActionResult Destroy(int Id)
+        {
+            return View();
+        }
+
+
+    }
+}
