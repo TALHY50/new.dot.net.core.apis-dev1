3bce4573166328a106fbf816a220fadfa352f9f0
Mahmud <al.mahmud@softrobotics.com.bd>
2024-04-02 16:54:49 +0600

    Page

--- ACL/Controllers/V1/AclPageController.cs
+++ ACL/Controllers/V1/AclPageController.cs
@@ -1,7 +1,7 @@
 ﻿using ACL.Database;
-using ACL.Database.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using ACL.Database.Models;
 
 namespace ACL.Controllers.V1
 {
@@ -15,65 +15,57 @@ namespace ACL.Controllers.V1
         {
             _context = context;
         }
-        [HttpGet(Name = "acl.company.list")]
-        [Route("pages")]
+
+        [HttpGet("pages", Name = "acl.company.list")]
         public async Task<IActionResult> Index()
         {
             var AclPage = await _context.AclPages.ToListAsync();
             return Ok(AclPage);
         }
 
-        [HttpGet(Name = "acl.page.add")]
-        [Route("pages/add")]
+        [HttpPost("pages/add", Name = "acl.page.add")]
         public IActionResult Create(AclPage AclPage)
         {
             _context.AclPages.Add(AclPage);
             _context.SaveChanges();
             return Ok(AclPage);
         }
-
-        [HttpGet(Name = "acl.page.edit")]
-        [Route("pages/edit/{id}")]
-        public IActionResult Edit(AclPage AclPage)
+        [HttpPut("pages/edit/{id}", Name = "acl.page.edit")]
+        public async Task<IActionResult> Edit(ulong id, AclPage AclPage)
         {
-            var _AclPage = _context.AclPages.FirstOrDefault(p => p.Id == AclPage.Id);
-            if (_AclPage == null)
+
+            if (ModelState.IsValid && id == AclPage.Id)
             {
-                return Ok("Not found");
+                _context.Entry(AclPage).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+                return Ok(AclPage);
             }
-            _AclPage = AclPage;
-            _context.AclPages.Update(AclPage);
-            var _acl_page = _context.SaveChanges();
-            return Ok(_AclPage);
+            return BadRequest("Id is not valid.");
+
         }
 
-        [HttpGet(Name = "acl.page.destroy")]
-        [Route("pages/delete/{id}")]
-        public IActionResult Destroy(AclPage AclPage)
+        [HttpDelete("pages/delete/{id}", Name = "acl.page.destroy")]
+        public IActionResult Destroy(ulong id)
         {
-            var _AclPage = _context.AclPages.FirstOrDefault(p => p.Id == AclPage.Id);
+            var _AclPage = _context.AclPages.FirstOrDefault(p => p.Id == id);
             if (_AclPage == null)
             {
                 return Ok("Not found");
             }
-            _context.AclPages.Remove(AclPage);
+            _context.AclPages.Remove(_AclPage);
             var _acl_page = _context.SaveChanges();
             return Ok("Remove page");
-        }       
-        
-        //[HttpGet(Name = "acl.page.view")]
-        //[Route("pages/view/{id}")]
-        //public IActionResult Show(AclPage AclPage)
-        //{
-        //    var _AclPage = _context.AclPages.FirstOrDefault(p => p.Id == AclPage.Id);
-        //    if (_AclPage == null)
-        //    {
-        //        return Ok("Not found");
-        //    }
-        //    return Ok(_AclPage);
-        //}
+        }
 
+        [HttpDelete("pages/view/{id}", Name = "acl.page.view")]
+        public IActionResult Show(ulong id)
+        {
+            var _AclPage = _context.AclPages.FirstOrDefault(p => p.Id == id);
+            if (_AclPage == null)
+            {
+                return Ok("Not found");
+            }
+            return Ok(_AclPage);
+        }
     }
-
-
 }
