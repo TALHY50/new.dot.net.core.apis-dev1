739a033c69cb5c4e9647ac2e833996fbf1f7ad92
Pulak Deb <pulak.deb@softrobotics.com.bd>
2024-04-02 16:54:37 +0600

    acl sub module crud

---
+++ ACL/Controllers/V1/AclSubModuleController.cs
@@ -0,0 +1,80 @@
+﻿using ACL.Database;
+using ACL.Database.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ACL.Controllers.V1
+{
+    [ApiController]
+    [Route("api/v1")]
+    public class AclSubModuleController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public AclSubModuleController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("submodules", Name = "acl.submodule.list")]
+        public async Task<IActionResult> Index()
+        {
+            var _AclSubModules = await _context.AclSubModules.ToListAsync();
+            return Ok(_AclSubModules);
+        }
+
+
+        [HttpPost("submodules/add",Name = "acl.submodule.add")]
+        public async Task<IActionResult> Create(AclSubModule objSubModule)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.AclSubModules.Add(objSubModule);
+                await _context.SaveChangesAsync();
+                return Ok(objSubModule);
+            }
+            return BadRequest(ModelState);
+
+        }
+
+        [HttpGet("submodules/view/{id}", Name = "acl.submodule.view")]
+        public async Task<IActionResult> View(ulong id)
+        {
+            var objSubModule = await _context.AclSubModules.FindAsync(id);
+            return Ok(objSubModule);
+
+        }
+
+
+        [HttpPut("submodules/edit/{id}", Name = "acl.submodule.edit")]
+        public async Task<IActionResult> Edit(ulong id, AclSubModule objSubModule)
+        {
+            if (ModelState.IsValid && id == objSubModule.Id)
+            {
+                _context.Entry(objSubModule).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+                return Ok(objSubModule);
+            }
+            return BadRequest("Id is not valid.");
+
+        }
+
+
+        [HttpDelete("submodules/delete/{id}", Name = "acl.submodule.destroy")]
+        public async Task<IActionResult> Destroy(ulong ID)
+        {
+            var subModule = _context.AclSubModules.Find(ID);
+            if (subModule != null)
+            {
+                _context.Entry(subModule).State = EntityState.Deleted;
+                await _context.SaveChangesAsync();
+                return  Ok("Sub Module Successfully Deleted.");
+            }
+
+            return BadRequest("Sub Module not found.");
+        }
+
+
+
+
+    }
+}
--- ACL/Database/Models/AclSubModule.cs
+++ ACL/Database/Models/AclSubModule.cs
@@ -1,27 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace ACL.Database.Models;
 
 public partial class AclSubModule
 {
+    [Required(ErrorMessage = "ID is required.")]
+    [Column("id")]
     public ulong Id { get; set; }
 
+    [Column("module_id")]
+    [Required(ErrorMessage = "Module ID is required.")]
     public ulong ModuleId { get; set; }
 
-    public string Name { get; set; } = null!;
+    [Column("name")]
+    [Required(ErrorMessage = "Name is required.")]
+    public  string Name { get; set; }
 
-    public string ControllerName { get; set; } = null!;
+    [Column("controller_name")]
+    [Required(ErrorMessage = "ControllerName is required.")]
+    public  string ControllerName { get; set; }
 
-    public string Icon { get; set; } = null!;
+    [Column("icon")]
+    [Required(ErrorMessage = "Icon is required.")]
+    public  string Icon { get; set; }
 
+    [Column("sequence")]
+    [Required(ErrorMessage = "Sequence is required.")]
     public int Sequence { get; set; }
 
+    [Column("default_method")]
+    [Required(ErrorMessage = "DefaultMethod is required.")]
+    public  string DefaultMethod { get; set; }
+
+    [Column("display_name")]
+    [Required(ErrorMessage = "DisplayName is required.")]
+    public  string DisplayName { get; set; }
+
+    [Column("created_at")]
     public DateTime? CreatedAt { get; set; }
 
+    [Column("updated_at")]
     public DateTime? UpdatedAt { get; set; }
-
-    public string DefaultMethod { get; set; } = null!;
-
-    public string DisplayName { get; set; } = null!;
 }
