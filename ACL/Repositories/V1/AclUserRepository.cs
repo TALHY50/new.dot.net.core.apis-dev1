c342547d357603cb7556ed6fbce6685ca831b023
Mahmud <al.mahmud@softrobotics.com.bd>
2024-04-08 17:04:39 +0600

    50% done on User

--- ACL/Repositories/V1/AclUserRepository.cs
+++ ACL/Repositories/V1/AclUserRepository.cs
@@ -3,7 +3,9 @@ using ACL.Interfaces;
 using ACL.Interfaces.Repositories.V1;
 using ACL.Requests.V1;
 using ACL.Response.V1;
+using Google.Protobuf.WellKnownTypes;
 using Microsoft.AspNetCore.Http.HttpResults;
+using SharedLibrary.Services;
 using SharedLibrary.Utilities;
 
 namespace ACL.Repositories.V1
@@ -42,6 +44,10 @@ namespace ACL.Repositories.V1
                 _unitOfWork.ApplicationDbContext.AddAsync(aclUser);
                 _unitOfWork.ApplicationDbContext.SaveChangesAsync();
                 _unitOfWork.ApplicationDbContext.Entry(aclUser).Reload();
+                var user_id= aclUser.Id;
+                // need to insert user user group
+
+
                 aclResponse.Data = aclUser;
                 aclResponse.Message = Helper.__(messageResponse.createMessage);
                 aclResponse.StatusCode = System.Net.HttpStatusCode.OK;
@@ -122,6 +128,20 @@ namespace ACL.Repositories.V1
             {
                 return new AclUser
                 {
+                    FirstName = request.first_name,
+                    LastName = request.last_name,
+                    Email = request.email,
+                    Password = Cryptographer.AppEncrypt( request.password),
+                    Avatar = request.avatar,
+                    Dob = request.dob,
+                    Gender = request.gender,
+                    Address = request.address,
+                    City = request.city,
+                    Country = request.country,
+                    Phone = request.phone,
+                    Username = request.username,
+                    ImgPath = request.img_path,
+                    Status = request.status,
                     CreatedAt = DateTime.Now,
                     UpdatedAt = DateTime.Now
                 };
--- ACL/Requests/V1/AclUserRequest.cs
+++ ACL/Requests/V1/AclUserRequest.cs
@@ -23,14 +23,13 @@ namespace ACL.Requests.V1
         public string avatar { get; set; }
 
         [Required]
-        [MaxLength(255)]
-        [MinLength(6)]
+        //[MaxLength(255)]
+        //[MinLength(6)]
         public string password { get; set; }
 
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime dob { get; set; }
-
 
         [Required]
         [Range(1, 4)]
@@ -39,28 +38,28 @@ namespace ACL.Requests.V1
         [MaxLength(255)]
         public string address { get; set; }
 
-        [MaxLength(100)]
+        //[MaxLength(100)]
         public string city { get; set; }
 
         public uint country { get; set; }
 
-        [MaxLength(20)]
+        //[MaxLength(20)]
         public string phone { get; set; }
 
-        [MaxLength(20)]
+        //[MaxLength(20)]
         public string username { get; set; }
 
-        [MaxLength(255)]
+        //[MaxLength(255)]
         public string img_path { get; set; }
 
         [Required]
-        [MaxLength(4)]
-        [MinLength(0)]
+        //[MaxLength(4)]
+        //[MinLength(0)]
         public sbyte status { get; set; }
 
         [Required]
-        [MinLength(1)]
-        public List<int> usergroup { get; set; }
+        [MinLength(1, ErrorMessage = "Array must contain at least one element.")]
+        public ulong[] usergroup { get; set; }
 
     }
 
