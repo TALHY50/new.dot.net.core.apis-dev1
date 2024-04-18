b50e4887ac634a1f78e962c176e4941c86e210da
siddique.porosh@softrobotics.com.bd <siddique.porosh@softrobotics.com.bd>
2024-04-18 16:42:35 +0600

    un wanted comment removed

--- src/ACL/Response/V1/AclResponse.cs
+++ src/ACL/Response/V1/AclResponse.cs
@@ -6,11 +6,11 @@ namespace ACL.Response.V1
     public class AclResponse
     {
         public HttpStatusCode StatusCode { get; set; }
-        //[DefaultValue("Data Not Found")]
-        public string Message { get; set; } //= "Data Not Found";
-        //[DefaultValue(null)]
+        [DefaultValue("Data Not Found")]
+        public string Message { get; set; } = "Data Not Found";
+        [DefaultValue(null)]
         public object Data { get; set; }
-        public DateTime Timestamp { get; set; } //= DateTime.Now;
+        public DateTime Timestamp { get; set; } = DateTime.Now;
 
     }
 }
