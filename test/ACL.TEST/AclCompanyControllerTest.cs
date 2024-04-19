db7b26b5d68ef0337e8dd3ba29d28bd79171b84f
siddique.porosh@softrobotics.com.bd <siddique.porosh@softrobotics.com.bd>
2024-04-19 18:14:28 +0600

    TestCase 1 For Company GetAll is Successfull

--- test/ACL.TEST/AclCompanyControllerTest.cs
+++ test/ACL.TEST/AclCompanyControllerTest.cs
@@ -21,7 +21,7 @@ public class AclCompanyControllerIntegrationTest
             // Populate the in-memory database with test data
             dbContext.AclCompanies.AddRange(new List<AclCompany>
             {
-                // Test data here...
+                new AclCompany{ Id = 1,AddedBy = 1,Address1 ="A",Address2 ="B",AverageTurnover=2.0,Cemail ="",City ="Dhaka",CmmiLevel = 1,Cname ="Porosh",Country="BD",Email="porosh@gmail.com",Fax="",Logo="",Name="Porosh",Phone="01672896992" ,Postcode ="1312",RegistrationNo="1234",State = "1",TaxNo="123456789",Timezone =1,TimezoneValue ="1",Status=1}
             });
             dbContext.SaveChanges();
         }
@@ -41,7 +41,7 @@ public class AclCompanyControllerIntegrationTest
             var res = result.Value as AclResponse;
             var returnCompanies = res.Data as IEnumerable<AclCompany>;
 
-            Assert.Equal(null, returnCompanies?.Count()); // Adjust count as per your test data
+            Assert.Equal(1, returnCompanies?.Count()); // Adjust count as per your test data
         }
     }
 }
