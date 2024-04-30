6f5c6e6b8a8fee82a0dbcc1c3d73aa595977f60e
Mahmud <al.mahmud@softrobotics.com.bd>
2024-04-30 19:03:03 +0600

    Module unit test

---
+++ test/ACL.TEST/V1/AclModuleUnitTest.cs
@@ -0,0 +1,135 @@
+using ACL.Requests;
+using ACL.Services;
+using Microsoft.EntityFrameworkCore;
+using RestSharp;
+using Bogus;
+using ACL.Database.Models;
+using ACL.Requests.V1;
+
+namespace ACL.Tests.V1
+{
+    public class AclModuleUnitTest
+    {
+        DatabaseConnector dbConnector;
+        CustomUnitOfWork unitOfWork;
+        RestClient restClient;
+        public AclModuleUnitTest()
+        {
+            dbConnector = new DatabaseConnector();
+            unitOfWork = new CustomUnitOfWork(dbConnector.dbContext);
+            unitOfWork.ApplicationDbContext = dbConnector.dbContext;
+            restClient = new RestClient(dbConnector.baseUrl);
+        }
+        [Fact]
+        public void TestModuleList()
+        {
+
+            //Arrange
+
+            // Act
+            var request = new RestRequest("modules", Method.Get);
+            //request.AddHeader("Authorization", "Bearer desc");
+
+            RestResponse response = restClient.Execute(request);
+
+
+            //// Assert
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(200, (int)response.StatusCode);
+
+        }
+        [Fact]
+        public void AddModuleTest()
+        {
+            //Arrange
+            var data = GetModule();
+
+            // Act
+            var request = new RestRequest("modules/add", Method.Post);
+            //request.AddHeader("Authorization", "Bearer desc");
+            request.AddJsonBody(data);
+
+            RestResponse response = restClient.Execute(request);
+
+
+            //// Assert
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(200, (int)response.StatusCode);
+
+        }
+
+        [Fact]
+        public void GetByIdModuleTest()
+        {
+            //Arrange
+            var id = getRandomID();
+
+            // Act
+            var request = new RestRequest($"modules/view/{id}", Method.Get);
+            //request.AddHeader("Authorization", "Bearer desc");
+
+
+            RestResponse response = restClient.Execute(request);
+
+
+            //// Assert
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(200, (int)response.StatusCode);
+
+        }
+        [Fact]
+        public void EditByIdModuleTest()
+        {
+            //Arrange
+
+            var data = GetModule();
+            var id = getRandomID();
+
+            // Act
+            var request = new RestRequest($"modules/edit/{id}", Method.Put);
+            //request.AddHeader("Authorization", "Bearer desc");
+            request.AddJsonBody(data);
+
+            RestResponse response = restClient.Execute(request);
+
+
+            //// Assert
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(200, (int)response.StatusCode);
+
+        }
+        [Fact]
+        public void DeleteByIdModuleTest()
+        {
+
+            var id = getRandomID();
+
+            // Act
+            var request = new RestRequest($"modules/delete/{id}", Method.Delete);
+            //request.AddHeader("Authorization", "Bearer desc");
+
+            RestResponse response = restClient.Execute(request);
+
+            // Assert
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(200, (int)response.StatusCode);
+
+        }
+
+        private AclModuleRequest GetModule()
+        {
+            var faker = new Faker();
+            return new AclModuleRequest
+            {
+                name = faker.Random.String2(10, 50),
+                icon = faker.Random.String2(1, 5),
+                sequence = faker.Random.Number(1, 3),
+                display_name = faker.Random.String2(1, 5)
+            };
+
+        }
+
+        private int getRandomID()
+        {
+
+            return (int)unitOfWork.AclModuleRepository.FirstOrDefault().Id;
+
+        }
+
+    }
+}
