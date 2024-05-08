9ce02886ce7d70d5d6c30cef5f438216b533e8ab
Pulak Deb <pulak.deb@softrobotics.com.bd>
2024-05-08 19:17:46 +0600

    global request handaling error message customize

--- src/ACL/Program.cs
+++ src/ACL/Program.cs
@@ -11,7 +11,10 @@ using SharedLibrary.Interfaces;
 using SharedLibrary.Services;
 using ACL.Data;
 using SharedLibrary.CustomMiddleWare;
-using Microsoft.EntityFrameworkCore.Diagnostics;
+using SharedLibrary.CustomDataAnotator;
+using Microsoft.AspNetCore.Mvc;
+using SharedLibrary.Response.CustomStatusCode;
+using Microsoft.Extensions.DependencyInjection;
 
 
 
@@ -81,6 +84,12 @@ builder.Services.AddSingleton<Serilog.ILogger>(_ => Log.Logger);
 builder.Services.AddSingleton<ILocalizationService>(new LocalizationService("ACL.Resources.en-US", typeof(Program).Assembly, "en-US"));
 builder.Services.AddSingleton<Microsoft.Extensions.Logging.ILogger>(_ => (Microsoft.Extensions.Logging.ILogger)Log.Logger);
 builder.Services.AddScoped<ILogService, LogService>();
+
+builder.Services.Configure<ApiBehaviorOptions>(o =>
+{
+    o.InvalidModelStateResponseFactory = actionContext =>
+    new BadRequestObjectResult(new { errors = new UnprocessableEntityObjectResult(actionContext.ModelState).Value, statusCode = AppStatusCode.FAIL });
+});
 
 Log.Logger = new LoggerConfiguration()
    .MinimumLevel.Debug()
