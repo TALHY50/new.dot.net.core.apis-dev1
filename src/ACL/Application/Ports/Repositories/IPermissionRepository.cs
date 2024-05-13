f0398831070b46437e9ce7c729c2fac91cb01ca8
rifat-simoom <rifat.simoom@softrobotics.com.bd>
2024-05-13 15:28:30 +0600

    refact! removed Permission as the entity, boundary is defined to User

--- src/ACL/Application/Ports/Repositories/IPermissionRepository.cs
+++
@@ -1,8 +0,0 @@
-using ACL.Domain.Permissions;
-
-namespace ACL.Application.Ports.Repositories;
-
-public interface IPermissionRepository
-{
-    public Task<Permission> GetPermissionAsync(uint userId, uint userPermissionVersion);
-}
