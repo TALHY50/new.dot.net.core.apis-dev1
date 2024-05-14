65c31aa43ea1d0ee7355a786630917aa89b49232
poroshdeveloper <siddique.porosh@softrobotics.com.bd>
2024-05-14 16:20:46 +0600

    Default Value Added For Auth

--- src/ACL/Application/UseCases/Login/Request/LoginRequest.cs
+++ src/ACL/Application/UseCases/Login/Request/LoginRequest.cs
@@ -1,8 +1,12 @@
-﻿namespace ACL.Application.UseCases.Login.Request
+﻿using System.ComponentModel;
+
+namespace ACL.Application.UseCases.Login.Request
 {
     public class LoginRequest : UseCases.Request
     {
+        [DefaultValue("ssadmin@sipay.com.tr")]
         public string Email { get; set; }
+        [DefaultValue("Nop@ss1234")]
         public string Password { get; set; }
     }
 }
