9c686be6ffee481eb9474b7511635f3fa0dadbe7
poroshdeveloper <siddique.porosh@softrobotics.com.bd>
2024-05-14 15:01:12 +0600

    Authentication And Authorization Added

--- src/ACL/Controllers/V1/AclPasswordController.cs
+++ src/ACL/Controllers/V1/AclPasswordController.cs
@@ -3,10 +3,12 @@ using System.ComponentModel;
 using ACL.Interfaces;
 using ACL.Requests.V1;
 using ACL.Response.V1;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ACL.Controllers.V1
 {
+    [Authorize]
     [Tags("Password")]
     [ApiController]
     public class AclPasswordController : ControllerBase
@@ -17,18 +19,23 @@ namespace ACL.Controllers.V1
             _unitOfWork = unitOfWork;
         }
 
+        [Authorize(Policy = "HasPermission")]
         [HttpPost(Route.AclRoutesUrl.AclPasswordRouteUrl.Reset, Name = Route.AclRoutesName.AclPasswordRouteNames.Reset)]
         public async Task<AclResponse> ResetPassword(AclPasswordResetRequest request)
         {
             return await _unitOfWork.AclPasswordRepository.Reset(request);
         }
+
+        [Authorize(Policy = "HasPermission")]
         [HttpPost(Route.AclRoutesUrl.AclPasswordRouteUrl.Forget, Name = Route.AclRoutesName.AclPasswordRouteNames.Forget)]
         public async Task<AclResponse> ForgetPassword(AclForgetPasswordRequest request)
         {
             return await _unitOfWork.AclPasswordRepository.Forget(request);
         }
+
+        [Authorize(Policy = "HasPermission")]
         [HttpPost(Route.AclRoutesUrl.AclPasswordRouteUrl.VerifyToken, Name = Route.AclRoutesName.AclPasswordRouteNames.VerifyToken)]
-        public async Task<AclResponse>VerifyTokenAndUpdatePassword(AclForgetPasswordTokenVerifyRequest request)
+        public async Task<AclResponse> VerifyTokenAndUpdatePassword(AclForgetPasswordTokenVerifyRequest request)
         {
             return await _unitOfWork.AclPasswordRepository.VerifyToken(request);
         }
--- src/ACL/Controllers/V1/AclUserGroupController.cs
+++ src/ACL/Controllers/V1/AclUserGroupController.cs
@@ -2,10 +2,12 @@
 using ACL.Requests.V1;
 using ACL.Response.V1;
 using ACL.Route;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ACL.Controllers.V1
 {
+    [Authorize]
     [Tags("User Group")]
     [ApiController]
     public class AclUserGroupController : Controller
@@ -16,29 +18,36 @@ namespace ACL.Controllers.V1
         {
             _unitOfWork = unitOfWork;
         }
+
+        [Authorize(Policy = "HasPermission")]
         [HttpGet(AclRoutesUrl.AclUserGroupRouteUrl.List, Name = AclRoutesName.AclUserGroupRouteNames.List)]
         public async Task<AclResponse> Index()
         {
             return await _unitOfWork.AclUserGroupRepository.GetAll();
         }
 
+        [Authorize(Policy = "HasPermission")]
         [HttpPost(AclRoutesUrl.AclUserGroupRouteUrl.Add, Name = AclRoutesName.AclUserGroupRouteNames.Add)]
         public async Task<AclResponse> Create(AclUserGroupRequest request)
         {
             return await _unitOfWork.AclUserGroupRepository.AddUserGroup(request);
         }
 
+        [Authorize(Policy = "HasPermission")]
         [HttpPut(AclRoutesUrl.AclUserGroupRouteUrl.Edit, Name = AclRoutesName.AclUserGroupRouteNames.Edit)]
         public async Task<AclResponse> Edit(ulong id, AclUserGroupRequest request)
         {
             return await _unitOfWork.AclUserGroupRepository.UpdateUserGroup(id, request);
         }
+
+        [Authorize(Policy = "HasPermission")]
         [HttpGet(AclRoutesUrl.AclUserGroupRouteUrl.View, Name = AclRoutesName.AclUserGroupRouteNames.View)]
         public async Task<AclResponse> View(ulong id)
         {
             return await _unitOfWork.AclUserGroupRepository.FindById(id);
         }
 
+        [Authorize(Policy = "HasPermission")]
         [HttpDelete(AclRoutesUrl.AclUserGroupRouteUrl.Destroy, Name = AclRoutesName.AclUserGroupRouteNames.Destroy)]
         public async Task<AclResponse> Destroy(ulong id)
         {
