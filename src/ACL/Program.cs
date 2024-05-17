47c679bde3733735bc3a88ec0198df1d076d9dec
poroshdeveloper <siddique.porosh@softrobotics.com.bd>
2024-05-18 01:51:16 +0600

    All Fixed And Pushed Plz Pull And Check As Fart As I Can Check i Found ok  And Removed All Warnings As Much As i Can

--- src/ACL/Program.cs
+++ src/ACL/Program.cs
@@ -231,13 +231,13 @@ builder.Host.UseSerilog((hostingContext, loggerConfiguration) =>
 builder.Services.AddSerilog();
 
 
-
+builder.Services.AddScoped<IAclBranchService,AclBranchService>();
 
 
 builder.Services.AddScoped<IAclUserRepository, AclUserRepository>();
-builder.Services.AddScoped<IAclBranchRepository, IAclBranchRepository>();
-builder.Services.AddScoped<IAclBranchService,AclBranchService>();
-builder.Services.AddScoped<IAclCompanyModuleRepository, IAclCompanyModuleRepository>();
+builder.Services.AddScoped<IAclBranchRepository, AclBranchRepository>();
+
+builder.Services.AddScoped<IAclCompanyModuleRepository, AclCompanyModuleRepository>();
 builder.Services.AddScoped<IAclCompanyRepository, AclCompanyRepository>();
 builder.Services.AddScoped<IAclCountryRepository, AclCountryRepository>();
 builder.Services.AddScoped<IAclModuleRepository, AclModuleRepository>();
