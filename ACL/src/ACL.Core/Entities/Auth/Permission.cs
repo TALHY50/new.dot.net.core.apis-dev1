d1eb7ecbe0b0efe899777cd2acac5d6e850602b0
rifat-simoom <rifat.simoom@softrobotics.com.bd>
2024-05-20 04:57:02 +0600

    feat! removed role based cache permission and integrated user id - version based permission

--- ACL/src/ACL.Core/Entities/Auth/Permission.cs
+++ ACL/src/ACL.Core/Entities/Auth/Permission.cs
@@ -5,8 +5,9 @@ public class Permission
     public uint Version { get; private set; }
     public HashSet<string>? RouteNames { get; private set; }
 
-    public Permission(HashSet<string>? routeNames)
+    public Permission(uint version, HashSet<string>? routeNames)
     {
+        this.Version = version;
         this.RouteNames = routeNames;
     }
 
