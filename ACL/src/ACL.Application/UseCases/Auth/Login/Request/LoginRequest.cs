1ee37df5a3ed9252cd6b712a0e13911e9ba1cd62
poroshdeveloper <siddique.porosh@softrobotics.com.bd>
2024-05-20 17:23:36 +0600

    Default Request Added back

--- ACL/src/ACL.Application/UseCases/Auth/Login/Request/LoginRequest.cs
+++ ACL/src/ACL.Application/UseCases/Auth/Login/Request/LoginRequest.cs
@@ -1,10 +1,12 @@
-﻿namespace ACL.Application.UseCases.Auth.Login.Request
+﻿using System.ComponentModel;
+
+namespace ACL.Application.UseCases.Auth.Login.Request
 {
     public class LoginRequest : UseCases.Request
     {
-       // [DefaultValue("ssadmin@sipay.com.tr")]
+        [DefaultValue("ssadmin@sipay.com.tr")]
         public string Email { get; set; }
-       // [DefaultValue("Nop@ss1234")]
+        [DefaultValue("Nop@ss1234")]
         public string Password { get; set; }
     }
 }
