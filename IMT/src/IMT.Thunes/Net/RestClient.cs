de75eb27120858c68d13896008e0e2b58920791f
Mahmud <al.mahmud@softrobotics.com.bd>
2024-06-06 12:42:22 +0600

    Modify Baseclient and restclient

--- IMT/src/IMT.Thunes/Net/RestClient.cs
+++ IMT/src/IMT.Thunes/Net/RestClient.cs
@@ -9,16 +9,6 @@ namespace IMT.Thunes.Net
 {
     public class RestClient : BaseRestClient
     {
-        public static CreateQuatationResponse Post(string url, Dictionary<string, string> headers, object request)
-        {
-            return ExchangeCreateQuatation(url, HttpMethod.Post, headers, null);
-        }
-
-        public static CreateQuatationResponse Get(string url, Dictionary<string, string> headers)
-        {
-            return ExchangeCreateQuatation(url, HttpMethod.Get, headers, null);
-        }
-
         public static T Get<T>(string url, Dictionary<string, string> headers)
         {
             return Exchange<T>(url, HttpMethod.Get, headers, null);
@@ -71,37 +61,5 @@ namespace IMT.Thunes.Net
                 throw e;
             }
         }
-
-        private static CreateQuatationResponse ExchangeCreateQuatation(string url, HttpMethod httpMethod, Dictionary<string, string> headers,
-            object request)
-        {
-            try
-            {
-                var requestMessage = BuildHttpRequestMessage(url, httpMethod, headers, request);
-                var httpResponseMessage = HttpClient.SendAsync(requestMessage).Result;
-                return HandleResponse(httpResponseMessage);
-            }
-            catch (_exception e)
-            {
-                throw new ThunesException(e);
-            }
-        }
-
-
-
-        //private static T Exchange<T>(string url, HttpMethod httpMethod, Dictionary<string, string> headers, object request)
-        //{
-        //    try
-        //    {
-        //        var requestMessage = BuildHttpRequestMessage(url, httpMethod, headers, request);
-        //        var httpResponseMessage = HttpClient.SendAsync(requestMessage).Result;
-        //        var content = httpResponseMessage.Content.ReadAsStringAsync().Result;
-        //        return HandleResponse<T>(httpResponseMessage, content);
-        //    }
-        //    catch (_exception e)
-        //    {
-        //        throw new ThunesException(e);
-        //    }
-        //}
     }
 }
