e234e9f3e4cf6c1e500f625f69b63dc6c527837a
poroshdeveloper <siddique.porosh@softrobotics.com.bd>
2024-06-07 19:09:01 +0600

    Url Fixed Permenently for transactions

--- IMT/src/IMT.Thunes/Route/ThunesUrl.cs
+++ IMT/src/IMT.Thunes/Route/ThunesUrl.cs
@@ -21,9 +21,9 @@ namespace IMT.Thunes.Route
         public const string RetrieveTransactionInformationByTransactionIdUrl = "/v2/money-transfer/transactions/{id}";
         public const string RetrieveTransactionInformationByExternalIdUrl = "/v2/money-transfer/transactions/ext-{external_id}";
         public const string ListAttachmentsOfATransactionByIdUrl = "/v2/money-transfer/transactions/{id}/attachments";
-        public const string ListAttachmentsOfTransactionByExternalIdUrl = "/v2/money-transfer/transactions/ext-{external-id}/attachments";
+        public const string ListAttachmentsOfTransactionByExternalIdUrl = "/v2/money-transfer/transactions/ext-{external_id}/attachments";
         public const string CancelTransactionByIdUrl = "/v2/money-transfer/transactions/{id}/cancel";
-        public const string CancelTransactionByExternalIdUrl = "/v2/money-transfer/transactions/{external-id}/cancel";
+        public const string CancelTransactionByExternalIdUrl = "/v2/money-transfer/transactions/{external_id}/cancel";
         public const string RetrieveAQuotationByIdUrl = "/v2/money-transfer/quotations/{id}";
         public const string RetrieveQuotationByExternalIdUrl = "/v2/money-transfer/quotations/ext-{external_id}";
         public const string CreditPartiesInformationUrl = "/v2/money-transfer/payers/{id}/{transaction_type}/credit-party-information";
