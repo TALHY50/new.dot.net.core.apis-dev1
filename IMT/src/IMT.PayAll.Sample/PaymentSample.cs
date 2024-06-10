50e84467baec73dd9cddc5e6775819cfa8d63e5a
Mahmud <al.mahmud@softrobotics.com.bd>
2024-06-10 14:29:49 +0600

    Payall and thunes sample class library added

---
+++ IMT/src/IMT.PayAll.Sample/PaymentSample.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace IMT.PayAll.Sample
+{
+    public class PaymentSample
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
