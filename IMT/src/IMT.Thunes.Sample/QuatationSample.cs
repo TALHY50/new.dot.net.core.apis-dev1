203423a1b2aff33530ecff0427e5f9780ce88505
Mahmud <al.mahmud@softrobotics.com.bd>
2024-06-10 16:14:17 +0600

    added test case sample not implemented

--- IMT/src/IMT.Thunes.Sample/QuatationSample.cs
+++ IMT/src/IMT.Thunes.Sample/QuatationSample.cs
@@ -2,15 +2,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DotNetEnv;
+using IMT.Thunes.Request.Transaction.Quoatation;
 using NUnit.Framework;
 
 namespace IMT.Thunes.Sample
 {
     public class QuatationSample
     {
+        private readonly ThunesClient _thunesClient = new ThunesClient("asdfasdf", "asdfasdf", "http://localhost:3001");
+
         [Test]
         public void Test1()
         {
+            CreateQuatationRequest request = new CreateQuatationRequest();
+            var response = _thunesClient.QuotationAdapter().CreateQuatatioin(request);
+
             Assert.True(true);
         }
     }
