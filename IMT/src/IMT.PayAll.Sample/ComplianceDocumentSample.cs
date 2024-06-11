3d8e4a64790e7f7fad97a00a4de40410f55cf186
poroshdeveloper <siddique.porosh@softrobotics.com.bd>
2024-06-11 19:37:48 +0600

    GetPaymentAccountsList Api Added In PayAll Sample Unit Test

---
+++ IMT/src/IMT.PayAll.Sample/ComplianceDocumentSample.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IMT.PayAll.Common;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using NUnit.Framework;
+
+namespace IMT.PayAll.Sample
+{
+    public class ComplianceDocumentSample
+    {
+        private readonly PayAllClient _payAllClient;
+        public ComplianceDocumentSample()
+        {
+            var requirement = BaseRequirement.GetBaseRequirement(BaseSample.ServerEnvironment);
+            _payAllClient = new PayAllClient(requirement.ClientID, requirement.ClientSecret, requirement.BaseUrl);
+        }
+
+        [Test]
+        public void GetPaymentAccountsList()
+        {
+            var content = Encoding.UTF8.GetBytes("This is a fake PDF content.");
+            var file = CreateFakeFormFile("Test.pdf", "application/pdf", content);
+            var result = _payAllClient.UploadNewComplianceDocument(file);
+            Assert.NotNull(result);
+        }
+
+        public static IFormFile CreateFakeFormFile(string fileName, string contentType, byte[] content)
+        {
+            var stream = new MemoryStream(content);
+            return new FormFile(stream, 0, content.Length, "name", fileName)
+            {
+                Headers = new HeaderDictionary(),
+                ContentType = contentType
+            };
+        }
+    }
+}
