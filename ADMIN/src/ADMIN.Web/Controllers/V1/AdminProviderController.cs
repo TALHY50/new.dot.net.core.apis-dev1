17a5a465929a59a8a35467e58b8068d7116ebcee
Mahmud <al.mahmud@softrobotics.com.bd>
2024-06-20 18:56:13 +0600

    Controller added

--- ADMIN/src/ADMIN.Web/Controllers/V1/AdminProviderController.cs
+++ ADMIN/src/ADMIN.Web/Controllers/V1/AdminProviderController.cs
@@ -2,6 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ADMIN.Application.Ports.Repositories.Interface;
+using ADMIN.Contracts.Requests;
+using ADMIN.Infrastructure.Persistence.Repositories.Provider;
+using ADMIN.Infrastructure.Route;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ADMIN.Web.Controllers.V1
@@ -10,8 +14,40 @@ namespace ADMIN.Web.Controllers.V1
     [ApiController]
     public class AdminProviderController : Controller
     {
+        private readonly IProviderRepository _repository;
+        public AdminProviderController(IProviderRepository repository)
+        {
+            _repository = repository;
+        }
 
+        [HttpGet(AdminRoutesUrl.AdminProviderUrl.List, Name = AdminRoutesNames.AdminProviderNames.List)]
+        public IActionResult Index()
+        {
+            return Ok(_repository.All());
+        }
+
+        [HttpPost(AdminRoutesUrl.AdminProviderUrl.Add, Name = AdminRoutesNames.AdminProviderNames.Add)]
+        public IActionResult Add(ProviderRequest request)
+        {
+            return Ok(_repository.Add(request));
+        }        
         
+        [HttpPut(AdminRoutesUrl.AdminProviderUrl.Edit, Name = AdminRoutesNames.AdminProviderNames.Edit)]
+        public IActionResult Edit(ulong id, ProviderRequest request)
+        {
+            return Ok(_repository.Update(id, request));
+        }        
         
+        [HttpGet(AdminRoutesUrl.AdminProviderUrl.Find, Name = AdminRoutesNames.AdminProviderNames.Find)]
+        public IActionResult Find(ulong id)
+        {
+            return Ok(_repository.Find(id));
+        }        
+        
+        [HttpDelete(AdminRoutesUrl.AdminProviderUrl.Delete, Name = AdminRoutesNames.AdminProviderNames.Delete)]
+        public IActionResult Delete(ulong id)
+        {
+            return Ok(_repository.Delete(id));
+        }
     }
 }
