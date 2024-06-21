0b2ac397b344a2539e5d137974987fbabe20fe56
mahmudul <mahmudul@noreply.example.org>
2024-06-21 19:51:15 +0000

    Update 'Dev/build.sh'

--- Dev/build.sh
+++ Dev/build.sh
@@ -1,5 +1,5 @@
 sudo rsync -avzh --progress "dev/" "/var/www/html/new.dot.net.core.apis"
 
-sudo cd /var/www/html/new.dot.net.core.apis/IMT/src/IMT.Web && sudo /usr/bin/dotnet build IMT.Web.csproj
+sudo /usr/bin/dotnet build /var/www/html/new.dot.net.core.apis/IMT/src/IMT.Web/IMT.Web.csproj
 
 sudo systemctl restart IMTWeb.service
