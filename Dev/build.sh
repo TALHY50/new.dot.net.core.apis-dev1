63c41dee8ea828e2fdc2f9d1288f52c36ea9acee
Pulak Deb <pulak.deb@softrobotics.com.bd>
2024-06-21 18:18:09 +0600

    autodeployment

--- Dev/build.sh
+++ Dev/build.sh
@@ -1,3 +1,3 @@
 sudo rsync -avzh --progress "dev/" "/root/new.dot.net.core.apis"
-sudo cd /root/new.dot.net.core.apis/IMT/src/IMT.Web
-sudo dotnet run
+#sudo cd /root/new.dot.net.core.apis/IMT/src/IMT.Web
+#sudo dotnet run
