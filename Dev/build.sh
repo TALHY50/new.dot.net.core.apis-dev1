52664e3c893b0fd8051ec7acdb74276ad4376917
hasib.hossen <hasib.hossen@softrobotics.com.bd>
2024-06-21 18:32:25 +0000

    Update 'Dev/build.sh'

--- Dev/build.sh
+++ Dev/build.sh
@@ -4,5 +4,5 @@ app="new.dot.net.core.apis/IMT/src/IMT.Web/"
 rdir="root"
 #cd /$rdir/$app
 sudo pkill dotnet
-sudo nohup dotnet run --project /$rdir/$app/IMT.Web.csproj
+#sudo nohup dotnet run --project /$rdir/$app/IMT.Web.csproj
 #sudo dotnet run
