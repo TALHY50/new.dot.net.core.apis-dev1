3c304938fc326aa2380fda43cd4c37f15babb68f
poroshdeveloper <siddique.porosh@softrobotics.com.bd>
2024-06-21 15:50:55 +0600

    Service added in admin provider and got expected response and status code from shared library

--- ADMIN/src/ADMIN.Infrastructure/Persistence/Repositories/Provider/ProviderRepository.cs
+++ ADMIN/src/ADMIN.Infrastructure/Persistence/Repositories/Provider/ProviderRepository.cs
@@ -10,61 +10,6 @@ namespace ADMIN.Infrastructure.Persistence.Repositories.Provider
     public class ProviderRepository(ApplicationDbContext dbContext)
         : GenericRepository<AdminProvider, ApplicationDbContext>(dbContext), IProviderRepository
     {
-        ///// <inheritdoc/>
-        //public AdminProvider? AddProvider(ProviderRequest request)
-        //{
-        //    AdminProvider adminProvider = PrepareData(request);
-        //   return base.Add(adminProvider);
-        //}
-        ///// <inheritdoc/>
-        //public AdminProvider? UpdateProvider(ulong id, ProviderRequest request)
-        //{
-        //    var adminProvider = base.GetById(id);
-        //    if (adminProvider != null)
-        //    {
-        //        AdminProvider prePareData = PrepareData(request, adminProvider);
-        //        return base.Update(prePareData);
-        //    }
-        //    return null;
-        //}
-        ///// <inheritdoc/>
-        //public AdminProvider? DeleteProvider(ulong id)
-        //{
-        //    var adminProvider = base.GetById(id);
-        //    if (adminProvider != null)
-        //    {
-        //        return (base.Delete(adminProvider).Result == true)?adminProvider:null;
-        //    }
-        //    else
-        //    {
-        //        return null;
-        //    }
-        //}
-
-        //public AdminProvider PrepareData(ProviderRequest request, AdminProvider? adminProvider = null)
-        //{
-        //    if (adminProvider != null)
-        //    {
-        //        adminProvider.Name = request.Name;
-        //        adminProvider.Code = request.Code;
-        //        adminProvider.BaseUrl = request.BaseUrl;
-        //        adminProvider.UpdatedBy = 1;
-        //        adminProvider.UpdatedAt = DateTime.Now;
-        //        return adminProvider;
-        //    }
-        //    else
-        //    {
-        //        return new AdminProvider
-        //        {
-        //            Name = request.Name,
-        //            Code = request.Code,
-        //            BaseUrl = request.BaseUrl,
-        //            CreatedBy = 1,
-        //            UpdatedBy = 1,
-        //            UpdatedAt = DateTime.Now,
-        //            CreatedAt = DateTime.Now,
-        //        };
-        //    }
-        //}
+        
     }
 }
