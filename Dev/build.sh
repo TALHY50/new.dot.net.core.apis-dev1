17e08fdd2dce66550d9800dc5868dde8ef1acfcb
galib <galib@noreply.example.org>
2024-06-21 13:52:06 +0000

    Update 'Dev/build.sh'

--- Dev/build.sh
+++ Dev/build.sh
@@ -2,6 +2,7 @@ sudo rsync -avzh --progress "dev/" "/root/new.dot.net.core.apis"
  
 app="new.dot.net.core.apis/IMT/src/IMT.Web/"
 rdir="root"
-cd /$rdir/$app
+#cd /$rdir/$app
 sudo pkill dotnet
-sudo dotnet run
+sudo nohup dotnet run --project /$rdir/$app/IMT.Web.csproj
+#sudo dotnet run
