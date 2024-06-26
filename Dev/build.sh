e6d15ede811b6f9e0097e855109541555da4f60e
hasib.hossen <hasib.hossen@softrobotics.com.bd>
2024-06-26 19:21:34 +0000

    Update 'Dev/build.sh'

--- Dev/build.sh
+++ Dev/build.sh
@@ -1,3 +1,5 @@
+sudo rm -rf /var/www/html/new.dot.net.core.apis/
+
 sudo rsync -avzh --progress "dev/" "/var/www/html/new.dot.net.core.apis"
 
 sudo /usr/bin/dotnet build /var/www/html/new.dot.net.core.apis/IMT/src/IMT.Web/IMT.Web.csproj
