c2a6d2b710e4c67bc4ca62bf9fe0ed2069c8f6f4
Mahmud <al.mahmud@softrobotics.com.bd>
2024-06-28 16:35:16 +0600

    Balance Response fixed

--- IMT/src/IMT.Thunes/Response/Account/BalanceResponse.cs
+++ IMT/src/IMT.Thunes/Response/Account/BalanceResponse.cs
@@ -9,9 +9,10 @@ namespace IMT.Thunes.Response.Account
     {
         public int id { get; set; }
         public string currency { get; set; }
-        public int balance { get; set; }
-        public int pending { get; set; }
-        public int available { get; set; }
+        public double balance { get; set; }
+        public int pending { get; set; } 
+        public double available { get; set; }
         public int credit_facility { get; set; }
+        public string? name { get; set; }
     }
 }
