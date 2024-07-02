6c2b751127b6c236a6f6c0353c336e236b21fdb3
poroshdeveloper <siddique.porosh@softrobotics.com.bd>
2024-07-02 12:46:39 +0600

    unnessesary classes removed and hardcoded baseurl api key introduce cause env not working in hosted one just to check aliveness in the hosted one

--- IMT/src/IMT.Web/Controllers/ThunesController.cs
+++ IMT/src/IMT.Web/Controllers/ThunesController.cs
@@ -17,7 +17,8 @@ namespace IMT.Web.Controllers
     [Route("[controller]")]
     public class ThunesController : ControllerBase
     {
-        private readonly ThunesClient _thunesClient = new(Env.GetString("THUNES_API_KEY"), Env.GetString("THUNES_API_SECRET"), Env.GetString("THUNES_BASE_URL"));
+      //  private readonly ThunesClient _thunesClient = new(Env.GetString("THUNES_API_KEY"), Env.GetString("THUNES_API_SECRET"), Env.GetString("THUNES_BASE_URL"));
+        private readonly ThunesClient _thunesClient = new(Env.GetString("f1c4a4d9-2899-4f09-b9f5-c35f09df5ffd"), Env.GetString("bed820bd-264b-4d0f-8148-9f56e0a8b55c"), Env.GetString("https://api-mt.pre.thunes.com"));
 
         [Tags("Thunes.Quatation")]
         [HttpPost(ThunesUrl.CreateQuatationUrl)]
