ba9e690bbb65087ef201b50ad6ad4bf6abc5f02e
poroshdeveloper <siddique.porosh@softrobotics.com.bd>
2024-07-03 17:00:50 +0600

    IMT modules Till Approved Added in shared library as domain entities

---
+++ SharedLibrary/Models/IMT/ImtCurrency.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SharedLibrary.Models.IMT;
+
+public partial class ImtCurrency
+{
+    public int Id { get; set; }
+
+    public string? Code { get; set; }
+
+    public string? IsoCode { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Symbol { get; set; }
+
+    public int? CreatedById { get; set; }
+
+    public int? UpdatedById { get; set; }
+
+    public sbyte? Status { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+}
---
+++ SharedLibrary/Models/IMT/ImtTransactionLimit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SharedLibrary.Models.IMT;
+
+public partial class ImtTransactionLimit
+{
+    public int Id { get; set; }
+
+    public sbyte? TransactionType { get; set; }
+
+    public sbyte? UserCategory { get; set; }
+
+    public int? DailyTotalNumber { get; set; }
+
+    public decimal? DailyTotalAmount { get; set; }
+
+    public int? MonthlyTotalNumber { get; set; }
+
+    public decimal? MonthlyTotalAmount { get; set; }
+
+    public int? CurrencyId { get; set; }
+
+    public int? CreatedById { get; set; }
+
+    public int? UpdatedById { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+}
