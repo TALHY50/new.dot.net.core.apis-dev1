c67465827bdcfc0d20aae61e0731f06359bc8e09
poroshdeveloper <siddique.porosh@softrobotics.com.bd>
2024-07-04 19:47:31 +0600

    Converting to a architectural pattern

---
+++ IMT/src/IMT.Application/Domain/Ports/Repositories/Quotation/IImtQuotationRepository.cs
@@ -0,0 +1,14 @@
+﻿using SharedLibrary.Models.IMT;
+using SharedLibrary.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMT.Application.Domain.Ports.Repositories.Quotation
+{
+    public interface IImtQuotationRepository : IGenericRepository<ImtQuotation>
+    {
+    }
+}
---
+++ IMT/src/IMT.Application/Infrastructure/Persistence/Repositories/ImtQuotationRepository.cs
@@ -0,0 +1,19 @@
+﻿using SharedLibrary.Interfaces;
+using SharedLibrary.Persistence.Configurations;
+using SharedLibrary.Models.IMT;
+using SharedLibrary.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMT.Application.Infrastructure.Persistence.Repositories
+{
+    public class ImtQuotationRepository : GenericRepository<ImtQuotation, ApplicationDbContext>
+    {
+        public ImtQuotationRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
