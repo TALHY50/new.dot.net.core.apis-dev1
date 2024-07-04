bd41d00f746ee61931fa5baa2066bab908ea2a07
poroshdeveloper <siddique.porosh@softrobotics.com.bd>
2024-07-04 16:37:00 +0600

    Transaction Controller Added And Imt.SLN edited and Shared Library Added

--- IMT/src/IMT.Web/Controllers/ThunesController.cs
+++ IMT/src/IMT.Web/Controllers/ThunesController.cs
@@ -81,16 +81,9 @@ namespace IMT.Web.Controllers
             {
                 return _thunesClient.VerificationAdapter().CreditPartyVerification(id, transaction_type, request);
             }
-            catch (System.Exception e)
+              catch (ThunesException e)
             {
-                if (e.Message == "Unauthorized")
-                {
-                    return Unauthorized();
-                }
-                else
-                {
-                    return NotFound();
-                }
+                return StatusCode(e.ErrorCode, e.Errors);
             }
         }
 
@@ -115,41 +108,41 @@ namespace IMT.Web.Controllers
 
         #region Transaction
 
-        [Tags("Thunes.Transaction")]
-        [HttpPost(ThunesUrl.CreateTransactionUrl)]
-        public Object TransactionPost(ulong id, MoneyTransferDTO request)
-        {
-            try
-            {
-                var transactionType = _thunesClient.QuotationAdapter().GetQuotationById(id);
-                if (request.IsValid(transactionType?.transaction_type?.ToLower()))
-                {
-                    return _thunesClient.GetTransactionAdapter().CreateTransaction(id, request);
-                }
-                return BadRequest("Request not valid");
-            }
-            catch (ThunesException e)
-            {
-                return StatusCode(e.ErrorCode, e.Errors);
-            }
-        }
-        [Tags("Thunes.Transaction")]
-        [HttpPost(ThunesUrl.CreateTransactionFromQuotationExternalIdUrl)]
-        public Object CreateTransactionFromQuotationExternalIdPost(int external_id, MoneyTransferDTO request)
-        {
-            try
-            {
-                if (request.IsValid(null))
-                {
-                    return _thunesClient.GetTransactionAdapter().CreateTransactionFromQuotationExternalId(external_id, request);
-                }
-                return BadRequest("Request not valid");
-            }
-            catch (ThunesException e)
-            {
-                return StatusCode(e.ErrorCode, e.Errors);
-            }
-        }
+        //[Tags("Thunes.Transaction")]
+        //[HttpPost(ThunesUrl.CreateTransactionUrl)]
+        //public Object TransactionPost(ulong id, MoneyTransferDTO request)
+        //{
+        //    try
+        //    {
+        //        var transactionType = _thunesClient.QuotationAdapter().GetQuotationById(id);
+        //        if (request.IsValid(transactionType?.transaction_type?.ToLower()))
+        //        {
+        //            return _thunesClient.GetTransactionAdapter().CreateTransaction(id, request);
+        //        }
+        //        return BadRequest("Request not valid");
+        //    }
+        //    catch (ThunesException e)
+        //    {
+        //        return StatusCode(e.ErrorCode, e.Errors);
+        //    }
+        //}
+        //[Tags("Thunes.Transaction")]
+        //[HttpPost(ThunesUrl.CreateTransactionFromQuotationExternalIdUrl)]
+        //public Object CreateTransactionFromQuotationExternalIdPost(int external_id, MoneyTransferDTO request)
+        //{
+        //    try
+        //    {
+        //        if (request.IsValid(null))
+        //        {
+        //            return _thunesClient.GetTransactionAdapter().CreateTransactionFromQuotationExternalId(external_id, request);
+        //        }
+        //        return BadRequest("Request not valid");
+        //    }
+        //    catch (ThunesException e)
+        //    {
+        //        return StatusCode(e.ErrorCode, e.Errors);
+        //    }
+        //}
 
         [Tags("Thunes.Transaction")]
         [HttpPost(ThunesUrl.CreateAttachmentToTransactionByIdUrl)]
---
+++ IMT/src/IMT.Web/Controllers/Transaction/TransactionController.cs
@@ -0,0 +1,51 @@
+﻿using IMT.Thunes.Exception;
+using IMT.Thunes.Request.Transaction.Transfer.CommonTransaction;
+using IMT.Thunes.Route;
+using IMT.Thunes;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IMT.Web.Controllers.Transaction
+{
+    [Tags("Thunes")]
+    [ApiController]
+    [Route("[controller]")]
+    public class TransactionController :BaseController
+    {
+        [Tags("Thunes.Transaction")]
+        [HttpPost(ThunesUrl.CreateTransactionUrl)]
+        public Object TransactionPost(ulong id, MoneyTransferDTO request)
+        {
+            try
+            {
+                var transactionType = _thunesClient.QuotationAdapter().GetQuotationById(id);
+                if (request.IsValid(transactionType?.transaction_type?.ToLower()))
+                {
+                    return _thunesClient.GetTransactionAdapter().CreateTransaction(id, request);
+                }
+                return BadRequest("Request not valid");
+            }
+            catch (ThunesException e)
+            {
+                return StatusCode(e.ErrorCode, e.Errors);
+            }
+        }
+        [Tags("Thunes.Transaction")]
+        [HttpPost(ThunesUrl.CreateTransactionFromQuotationExternalIdUrl)]
+        public Object CreateTransactionFromQuotationExternalIdPost(int external_id, MoneyTransferDTO request)
+        {
+            try
+            {
+                if (request.IsValid(null))
+                {
+                    return _thunesClient.GetTransactionAdapter().CreateTransactionFromQuotationExternalId(external_id, request);
+                }
+                return BadRequest("Request not valid");
+            }
+            catch (ThunesException e)
+            {
+                return StatusCode(e.ErrorCode, e.Errors);
+            }
+        }
+    }
+}
