5d7e7254f9ec7006dc05dfc7d5c997dc14fb5618
Mahmud <al.mahmud@softrobotics.com.bd>
2024-07-08 16:12:19 +0600

    Send Money

--- IMT/src/IMT.Application/Contracts/Requests/SendMoiney/SendMoneyRequest.cs
+++ IMT/src/IMT.Application/Contracts/Requests/SendMoiney/SendMoneyRequest.cs
@@ -1,3 +1,4 @@
+using IMT.Thunes.Request.Transaction.Transfer.CommonTransaction;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -42,59 +43,18 @@ namespace IMT.Application.Contracts.Requests.SendMoiney
         // Create quoatation request End
 
         // create money transfer request start
-        public int Id { get; set; }
+        public CreditPartyIdentifier? credit_party_identifier { get; set; }
+        public Beneficiary? beneficiary { get; set; }
+        public SendingBusiness? sending_business { get; set; }
+        public Sender? sender { get; set; }
+        public ReceivingBusinesss? receiving_business { get; set; }
+        public string? document_reference_number { get; set; }
+        public string? purpose_of_remittance { get; set; }
 
-        public string? PaymentId { get; set; }
+        public string? callback_url { get; set; }
+        public string? retail_fee_currency { get; set; }
+        public double? retail_fee { get; set; }
 
-        public int? TransactionStateId { get; set; }
-
-        public int? ReasonId { get; set; }
-
-        public int? PaymentMethodId { get; set; }
-
-        /// <summary>
-        /// 1=instant, 2=regular, 3=same_day etc.
-        /// </summary>
-        public sbyte? TransferType { get; set; }
-
-        public string? ReasonCode { get; set; }
-
-        /// <summary>
-        /// type 1 = b2b, 2 = c2c, 3=c2b, 4=b2c etc
-        /// </summary>
-        public sbyte? Type { get; set; }
-
-        public string? SenderName { get; set; }
-
-        public string? ReceiverName { get; set; }
-
-        public int? SenderCurrencyId { get; set; }
-
-        public int? ReceiverCurrencyId { get; set; }
-
-        public decimal? ExchangeRate { get; set; }
-
-        public decimal? SendAmount { get; set; }
-
-        public decimal? ReceiveAmount { get; set; }
-
-        public decimal? ExchangedAmount { get; set; }
-
-        public decimal? Fee { get; set; }
-
-        public decimal? Vat { get; set; }
-
-        public string? CommissionPaidBy { get; set; }
-
-        public int? SenderCustomerId { get; set; }
-
-        public int? ReceiverCustomerId { get; set; }
-
-        public string? Source { get; set; }
-
-        public string? RemoteOrderId { get; set; }
-
-        public string? InvoiceId { get; set; }
         // create money transfer request end
 
     }
