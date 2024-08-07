6e4b2f0da84e9bddf6cd741765f5d6c9241e08ce
poroshdeveloper <siddique.porosh@softrobotics.com.bd>
2024-08-07 12:42:22 +0600

    Callback endpoint added

---
+++ IMT/src/IMT.App/Application/Features/CallbackController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Thunes.Request.Transaction.Transfer.CommonTransaction;
+
+namespace IMT.App.Application.Features
+{
+    public class CallbackController : Controller
+    {
+        [HttpPost("/api/v1/callback")]
+        public IActionResult Callback(MoneyTransferDTO transferDTO)
+        {
+            return View();
+        }
+    }
+}
