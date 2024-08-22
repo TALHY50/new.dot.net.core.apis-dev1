25900e535d30a0677222bdcab8a9f141a872270d
poroshdeveloper <siddique.porosh@softrobotics.com.bd>
2024-08-22 18:49:17 +0600

    Features Added In Admin Project And MttsController Added

---
+++ ADMIN/src/Admin.App/Features/MttsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SharedKernel.Main.Application.Common;
+
+namespace ADMIN.App.Features
+{
+    public class MttsController : ApiControllerBase
+    {
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
