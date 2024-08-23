7854e498e64bb0bd9b3646bb263c495fcccf9c63
Mahmud <al.mahmud@softrobotics.com.bd>
2024-08-23 12:43:24 +0600

    Added Holiday Setting

---
+++ ADMIN/src/Admin.App/Application/Features/HolidaySetting/GetHolidaySettings.cs
@@ -0,0 +1,37 @@
+﻿using Ardalis.SharedKernel;
+using ErrorOr;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SharedKernel.Main.Application.Common;
+using SharedKernel.Main.Application.Common.Constants;
+using SharedKernel.Main.Domain.Admin;
+
+namespace ADMIN.App.Application.Features.HolidaySetting;
+
+public class GetHolidaySettingsController : ApiControllerBase
+{
+    // [Authorize(Policy = "HasPermission")]
+    [HttpGet(Routes.GetHolidaySettingUrl, Name = Routes.GetHolidaySettingName)]
+    public async Task<ActionResult<ErrorOr<BusinessHoursAndWeekends>>> Get()
+    {
+        return await Mediator.Send(new GetHolidaySettingQuery()).ConfigureAwait(false);
+    }
+
+    public record GetHolidaySettingQuery() : IQuery<ErrorOr<BusinessHoursAndWeekends>>;
+
+    internal sealed class GetHolidaySettingsHandler() : IQueryHandler<GetHolidaySettingQuery, ErrorOr<BusinessHoursAndWeekends>>
+    {
+        // get all data 
+        public Task<ErrorOr<BusinessHoursAndWeekends>> Handle(GetHolidaySettingQuery request, CancellationToken cancellationToken)
+        {
+            // ToDo
+            // Get all item from db
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+
+}
---
+++ ADMIN/src/Admin.App/Application/Features/HolidaySetting/UpdateHolidaySetting.cs
@@ -0,0 +1,50 @@
+﻿using ErrorOr;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SharedKernel.Main.Application.Common;
+using SharedKernel.Main.Application.Common.Constants;
+using SharedKernel.Main.Domain.Admin;
+
+namespace ADMIN.App.Application.Features.HolidaySetting;
+
+public class UpdateHolidaySettingController : ApiControllerBase
+{
+    //[Authorize(Policy = "HasPermission")]
+    [HttpPut(Routes.UpdateHolidaySettingUrl, Name = Routes.UpdateHolidaySettingName)]
+    public async Task<ActionResult<ErrorOr<BusinessHoursAndWeekends>>> Create(UpdateHolidaySettingCommand command)
+    {
+        return await Mediator.Send(command).ConfigureAwait(false);
+    }
+
+    public record UpdateHolidaySettingCommand(int id, byte HourType, int? CountryId,
+    string Day, byte IsWeekend, sbyte Gmt, DateTime OpenAt, DateTime CloseAt, int CompanyId, byte Status)
+    : IRequest<ErrorOr<BusinessHoursAndWeekends>>;
+
+
+    internal sealed class UpdateHolidaySettingCommandValidator : AbstractValidator<UpdateHolidaySettingCommand>
+    {
+        public UpdateHolidaySettingCommandValidator()
+        {
+            RuleFor(r => r.id).NotEmpty();
+        }
+    }
+
+    internal sealed class UpdateHolidaySettingHandler() : IRequestHandler<UpdateHolidaySettingCommand, ErrorOr<BusinessHoursAndWeekends>>
+    {
+        public Task<ErrorOr<BusinessHoursAndWeekends>> Handle(UpdateHolidaySettingCommand request, CancellationToken cancellationToken)
+        {
+
+            // ToDo
+            // First get the item from db
+            // Then update current item
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+
+
+
+}
