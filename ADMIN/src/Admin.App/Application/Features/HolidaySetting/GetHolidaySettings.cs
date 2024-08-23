6c15ec024590555c39fbc20b7e486277d9e06955
Mahmud <al.mahmud@softrobotics.com.bd>
2024-08-23 20:25:01 +0600

    Bussiness hour and holiday crud.

--- ADMIN/src/Admin.App/Application/Features/HolidaySetting/GetHolidaySettings.cs
+++ ADMIN/src/Admin.App/Application/Features/HolidaySetting/GetHolidaySettings.cs
@@ -1,8 +1,9 @@
 ﻿using Ardalis.SharedKernel;
-using ErrorOr;
+using Ardalis.Specification.EntityFrameworkCore;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SharedKernel.Main.Application.Common;
 using SharedKernel.Main.Application.Common.Constants;
 using SharedKernel.Main.Application.Interfaces.Repositories.Admin;
@@ -16,21 +17,22 @@ public class GetHolidaySettingsController : ApiControllerBase
 {
     // [Authorize(Policy = "HasPermission")]
     [HttpGet(Routes.GetHolidaySettingUrl, Name = Routes.GetHolidaySettingName)]
-    public async Task<ActionResult<ErrorOr<Entities.HolidaySetting>>> Get()
+    public async Task<ActionResult<List<Entities.HolidaySetting>>> Get()
     {
         return await Mediator.Send(new GetHolidaySettingQuery()).ConfigureAwait(false);
     }
 
-    public record GetHolidaySettingQuery() : IQuery<ErrorOr<Entities.HolidaySetting>>;
+    public record GetHolidaySettingQuery() : IQuery<List<Entities.HolidaySetting>>;
 
-    internal sealed class GetHolidaySettingsHandler(ImtApplicationDbContext context, IHolidaySettingRepository repository) : IQueryHandler<GetHolidaySettingQuery, ErrorOr<Entities.HolidaySetting>>
+    internal sealed class GetHolidaySettingsHandler(ImtApplicationDbContext _context, IHolidaySettingRepository repository) : IQueryHandler<GetHolidaySettingQuery, List<Entities.HolidaySetting>>
     {
         // get all data 
-        public Task<ErrorOr<Entities.HolidaySetting>> Handle(GetHolidaySettingQuery request, CancellationToken cancellationToken)
+        public async Task<List<Entities.HolidaySetting>> Handle(GetHolidaySettingQuery request, CancellationToken cancellationToken)
         {
             // ToDo
             // Get all item from db
-            throw new NotImplementedException();
+            var holidaySetting = await _context.ImtHolidaySettings.ToListAsync(cancellationToken);
+            return holidaySetting;
         }
     }
 
