b502798a52ae910d2fd1ab3fbf9ca08e5e06910f
rifat-simoom <rifat.simoom@softrobotics.com.bd>
2024-08-31 20:44:53 +0600

    feat! added pagination

--- SharedKernel/src/SharedKernel.Main/ConfigureServices.cs
+++ SharedKernel/src/SharedKernel.Main/ConfigureServices.cs
@@ -8,6 +8,7 @@ public static class ConfigureServices
 {
     public static IServiceCollection AddSharedKernel(this IServiceCollection services)
     {
+        DotNetEnv.Env.Load();
         Infrastructure.DependencyInjection.AddInfrastructure(services);
         Application.DependencyInjection.AddApplication(services);
         Presentation.DependencyInjection.AddPresentation(services);
---
+++ SharedKernel/src/SharedKernel.Main/Contracts/Common/PaginatorRequest.cs
@@ -0,0 +1,7 @@
+namespace SharedKernel.Main.Contracts.Common;
+
+public class PaginatorRequest
+{
+    public int page_number { get; set; }
+    public int page_size { get; set; }
+}
