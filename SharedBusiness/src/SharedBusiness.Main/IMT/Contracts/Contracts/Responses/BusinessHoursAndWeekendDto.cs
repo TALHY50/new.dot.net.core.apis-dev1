c9b97515e2b04522fbea1069a40d9d242aa69434
Mahmud <al.mahmud@softrobotics.com.bd>
2024-09-02 19:01:19 +0600

    Merge

--- SharedBusiness/src/SharedBusiness.Main/IMT/Contracts/Contracts/Responses/BusinessHoursAndWeekendDto.cs
+++
@@ -1,6 +0,0 @@
-namespace SharedBusiness.Main.IMT.Contracts.Contracts.Responses;
-
-public record BusinessHoursAndWeekendDto(
-    uint id, byte HourType, uint? CountryId,
-    string Day, sbyte IsWeekend, string Gmt, DateTime OpenAt, DateTime CloseAt, uint? CompanyId, byte Status
-    );
