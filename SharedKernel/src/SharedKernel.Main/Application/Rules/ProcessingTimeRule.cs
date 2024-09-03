3b672212359453b634d7b76af81652c05943fb4b
Ashraful Alam <ashraful.alam@softrobotics.com.bd>
2024-09-03 16:07:21 +0600

    some namespace fixed

--- SharedKernel/src/SharedKernel.Main/Application/Rules/ProcessingTimeRule.cs
+++ SharedKernel/src/SharedKernel.Main/Application/Rules/ProcessingTimeRule.cs
@@ -1,10 +1,5 @@
-﻿]using FluentValidation;
+﻿using FluentValidation;
 using SharedKernel.Main.Contracts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SharedKernel.Main.Application.Rules
 {
