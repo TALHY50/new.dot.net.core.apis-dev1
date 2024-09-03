859cea77612ac41f5aa98ff6fae14f6a42e53bbc
Alamin <amialaminb@gmail.com>
2024-09-03 11:24:46 +0600

    commit staged

--- Admin/src/Admin.App/Infrastructure/AdminInfrastructureDependencyInjection.cs
+++ Admin/src/Admin.App/Infrastructure/AdminInfrastructureDependencyInjection.cs
@@ -1,3 +1,15 @@
+using System.Security.Cryptography;
+using ACL.Business.Application.Interfaces.Repositories;
+using ACL.Business.Application.Interfaces.Services;
+using ACL.Business.Infrastructure.Persistence.Context;
+using ACL.Business.Infrastructure.Persistence.Repositories;
+using ACL.Business.Infrastructure.Security;
+using Admin.App.Application.Features.Corridors;
+using Admin.App.Application.Features.Currencies;
+using Admin.App.Application.Features.Payers;
+using Admin.App.Application.Features.Providers;
+using Admin.App.Application.Features.Regions;
+using Admin.App.Application.Features.TransactionTypes;
 using Admin.App.Infrastructure.Persistence;
 
 namespace Admin.App.Infrastructure;
