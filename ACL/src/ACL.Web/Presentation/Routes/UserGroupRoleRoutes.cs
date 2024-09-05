b7f82ef235055602d2a60b1d5e322f6e767b6fa0
Ashraful Alam <ashraful.alam@softrobotics.com.bd>
2024-09-05 13:56:13 +0600

    API endpoints naming convention fixed for Admin web and ACL web(usergroupRole)

--- ACL/src/ACL.Web/Presentation/Routes/UserGroupRoleRoutes.cs
+++ ACL/src/ACL.Web/Presentation/Routes/UserGroupRoleRoutes.cs
@@ -2,24 +2,24 @@
 {
     public class UserGroupRoleRoutes
     {
-        public const string GetUserGroupRoleMethod = "GET";
-        public const string GetUserGroupRoleName = "user_group_roles";
-        public const string GetUserGroupRoleTemplate = "/user_group_roles";
+        //public const string GetUserGroupRoleMethod = "GET";
+        //public const string GetUserGroupRoleName = "user-group-roles";
+        //public const string GetUserGroupRoleTemplate = "/user-group-roles";
 
         public const string GetUserGroupRoleByUserGroupIdMethod = "GET";
-        public const string GetUserGroupRoleByUserGroupIdName = "get_user_group_role_by_user_group_id";
-        public const string GetUserGroupRoleByUserGroupIdTemplate = "/user_group_roles/{user_group_id}";
+        public const string GetUserGroupRoleByUserGroupIdName = "get_usergroup_role_by_user_group_id";
+        public const string GetUserGroupRoleByUserGroupIdTemplate = "/usergroups/roles/{user_group_id}";
 
-        public const string CreateUserGroupRoleMethod = "POST";
-        public const string CreateUserGroupRoleName = "create_user_group_role";
-        public const string CreateUserGroupRoleTemplate = "/user_group_roles";
+        //public const string CreateUserGroupRoleMethod = "POST";
+        //public const string CreateUserGroupRoleName = "create-user-group-role";
+        //public const string CreateUserGroupRoleTemplate = "/user-group-roles";
 
-        public const string DeleteUserGroupRoleMethod = "DELETE";
-        public const string DeleteUserGroupRoleName = "delete_user_group_role_by_id";
-        public const string DeleteUserGroupRoleTemplate = "/user_group_roles/{id}";
+        //public const string DeleteUserGroupRoleMethod = "DELETE";
+        //public const string DeleteUserGroupRoleName = "delete-user-group-role-by-id";
+        //public const string DeleteUserGroupRoleTemplate = "/user-group-roles/{id}";
 
         public const string UpdateUserGroupRoleMethod = "PUT";
-        public const string UpdateUserGroupRoleName = "update_user_group_roles";
-        public const string UpdateUserGroupRoleTemplate = "/user_group_roles";
+        public const string UpdateUserGroupRoleName = "update_usergroup_role";
+        public const string UpdateUserGroupRoleTemplate = "/usergroups/roles";
     }
 }
--- Admin/src/Admin.Web/Presentation/Routes/TransactionTypeRoutes.cs
+++ Admin/src/Admin.Web/Presentation/Routes/TransactionTypeRoutes.cs
@@ -3,23 +3,23 @@
     public class TransactionTypeRoutes
     {
         public const string GetTransactionTypeMethod = "GET";
-        public const string GetTransactionTypeName = "transactionTypes";
-        public const string GetTransactionTypeTemplate = "/transactionTypes";
+        public const string GetTransactionTypeName = "transaction-types";
+        public const string GetTransactionTypeTemplate = "/transaction-types";
 
         public const string GetTransactionTypeByIdMethod = "GET";
-        public const string GetTransactionTypeByIdName = "get_transactionType_by_id";
-        public const string GetTransactionTypeByIdTemplate = "/transactionTypes/{id}";
+        public const string GetTransactionTypeByIdName = "get-transaction-type-by-id";
+        public const string GetTransactionTypeByIdTemplate = "/transaction-types/{id}";
 
         public const string CreateTransactionTypeMethod = "POST";
-        public const string CreateTransactionTypeName = "create_transactionType";
-        public const string CreateTransactionTypeTemplate = "/transactionTypes";
+        public const string CreateTransactionTypeName = "create-transaction-type";
+        public const string CreateTransactionTypeTemplate = "/transaction-types";
 
         public const string DeleteTransactionTypeMethod = "DELETE";
-        public const string DeleteTransactionTypeName = "delete_transactionType_by_id";
-        public const string DeleteTransactionTypeTemplate = "/transactionTypes/{id}";
+        public const string DeleteTransactionTypeName = "delete_transaction-type-by-id";
+        public const string DeleteTransactionTypeTemplate = "/transaction-types/{id}";
 
         public const string UpdateTransactionTypeMethod = "PUT";
-        public const string UpdateTransactionTypeName = "update_transactionType_by_id";
-        public const string UpdateTransactionTypeTemplate = "/transactionTypes/{id}";
+        public const string UpdateTransactionTypeName = "update_transaction-type-by-id";
+        public const string UpdateTransactionTypeTemplate = "/transaction-types/{id}";
     }
 }
