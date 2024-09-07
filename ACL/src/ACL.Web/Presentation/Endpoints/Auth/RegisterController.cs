301ec3607d5b8f8c44b0a55958d38f1cde9d360c
Bassam Rahat <bassamrahat.ibs@gmail.com>
2024-09-07 23:12:32 +0500

    chore: register endpoint sorted

--- ACL/src/ACL.Web/Presentation/Endpoints/Auth/RegisterController.cs
+++ ACL/src/ACL.Web/Presentation/Endpoints/Auth/RegisterController.cs
@@ -1,80 +1,137 @@
+using ACL.Business.Application.Features.Users;
+using ACL.Business.Application.Interfaces.Repositories;
 using ACL.Business.Domain.Entities;
+using ACL.Business.Infrastructure.Auth.Auth;
 using ACL.Web.Presentation.Routes;
 using Microsoft.AspNetCore.Authentication.BearerToken;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using SharedKernel.Main.Application.Interfaces.Services;
 
 namespace ACL.Web.Presentation.Endpoints.Auth;
 
-
-/// <summary>
-/// 
-/// </summary>
-[ApiExplorerSettings(IgnoreApi = true)]
 public class RegisterController : AuthBaseController
 {
-    /// <inheritdoc />
+    private readonly IUserRepository _userRepository;
+    private readonly UserManager<User> _userManager;
+    private readonly IUserEmailStore<User> _emailStore;
+    private readonly ICryptography _cryptography;
+    private readonly IUserUserGroupRepository _userUserGroupRepository;
+
     public RegisterController(
-        ILogger<RegisterController> logger, 
+        ILogger<RegisterController> logger,
         ICurrentUser currentUser,
         IServiceProvider serviceProvider,
         TimeProvider timeProvider,
         IOptionsMonitor<BearerTokenOptions> bearerTokenOptions,
         IEmailSender<User> emailSender,
         LinkGenerator linkGenerator,
-        IHttpContextAccessor context
-        )
-        : base(logger, currentUser, serviceProvider, timeProvider, bearerTokenOptions, emailSender, linkGenerator, context)
+        IHttpContextAccessor context,
+        IUserRepository userRepository,
+        ICryptography cryptography,
+        IUserUserGroupRepository userUserGroupRepository
+    ) : base(logger, currentUser, serviceProvider, timeProvider, bearerTokenOptions, emailSender, linkGenerator, context)
     {
-        _userManager = _serviceProvider.GetRequiredService<UserManager<User>>();
+        _userManager = serviceProvider.GetRequiredService<UserManager<User>>()
+            ?? throw new ArgumentNullException(nameof(UserManager<User>));
 
         if (!_userManager.SupportsUserEmail)
         {
             throw new NotSupportedException($"{nameof(RegisterController)} requires a user store with email support.");
         }
 
-        _userStore = _serviceProvider.GetRequiredService<IUserStore<User>>();
-        _emailStore = (IUserEmailStore<User>)_userStore;
+        var userStore = serviceProvider.GetRequiredService<IUserStore<User>>()
+            ?? throw new ArgumentNullException(nameof(IUserStore<User>));
+
+        _emailStore = userStore as IUserEmailStore<User>
+            ?? throw new InvalidCastException($"{nameof(RegisterController)} requires a user store that supports email.");
+
+        _userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
+        _cryptography = cryptography ?? throw new ArgumentNullException(nameof(cryptography));
+        _userUserGroupRepository = userUserGroupRepository ?? throw new ArgumentNullException(nameof(userUserGroupRepository));
     }
-    
 
     [Tags("Auth")]
     [HttpPost(AuthRoutes.RegisterTemplate, Name = AuthRoutes.RegisterName)]
-    public async Task<ActionResult> Register([FromBody] RegisterRequest registration)
+    public async Task<ActionResult> Register([FromBody] CreateUserCommand command)
     {
-        var email = registration.Email;
-
-        if (string.IsNullOrEmpty(email) || !_emailAddressAttribute.IsValid(email))
+        if (_userRepository.IsAclUserEmailExist(command.email))
         {
-            return CreateValidationProblem(IdentityResult.Failed(_userManager.ErrorDescriber.InvalidEmail(email)));
+            return BadRequest("Email already exists.");
         }
 
-        var user = new User();
-        await _userStore.SetUserNameAsync(user, email, CancellationToken.None);
-        await _emailStore.SetEmailAsync(user, email, CancellationToken.None);
-        var result = await _userManager.CreateAsync(user, registration.Password);
+        var salt = _cryptography.GenerateSalt();
+
+        var user = new User
+        {
+            FirstName = command.first_name,
+            LastName = command.last_name,
+            Email = command.email,
+            UserName = command.user_name,
+            PasswordHash = _cryptography.HashPassword(command.password, salt),
+            Avatar = command.avatar,
+            Dob = command.dob,
+            Gender = command.gender,
+            Address = command.address,
+            City = command.city,
+            Country = command.country ?? 0,
+            PhoneNumber = command.phone,
+            Language = command.language,
+            ImgPath = command.img_path,
+            Status = (sbyte)command.status,
+
+            Salt = salt,
+            CompanyId = (uint)AppAuth.GetAuthInfo().CompanyId,
+            UserType = ((uint)AppAuth.GetAuthInfo().CompanyId == 0) ? (uint)1 : (uint)2, // Admin or User
+            EmailConfirmed = false,
+            SecurityStamp = salt,
+            ConcurrencyStamp = salt,
+            TwoFactorEnabled = false,
+            LockoutEnabled = false,
+            AccessFailedCount = 0,
+            CreatedAt = DateTime.Now,
+            UpdatedAt = DateTime.Now
+        };
+
+        var result = await _userManager.CreateAsync(user, command.password);
 
         if (!result.Succeeded)
         {
             return CreateValidationProblem(result);
         }
 
-        //await SendConfirmationEmailAsync(user, _userManager, _context.HttpContext, email);
+        if (command.user_group != null && command.user_group.Length > 0)
+        {
+            var userUserGroups = PrepareDataForUserUserGroups(command, user.Id);
+            await _userUserGroupRepository.AddRangeAsync(userUserGroups);
+        }
+
         return Ok();
     }
 
     private ActionResult CreateValidationProblem(IdentityResult result)
     {
-        var errors = result.Errors;
-        foreach (var error in errors)
+        foreach (var error in result.Errors)
         {
             ModelState.AddModelError(string.Empty, error.Description);
         }
         return ValidationProblem(ModelState);
     }
 
-   
+    private UserUsergroup[] PrepareDataForUserUserGroups(CreateUserCommand command, uint? userId)
+    {
+        if (userId == null)
+        {
+            throw new ArgumentNullException(nameof(userId));
+        }
+
+        return command.user_group.Select(userGroup => new UserUsergroup
+        {
+            UserId = userId.Value,
+            UsergroupId = userGroup,
+            CreatedAt = DateTime.Now,
+            UpdatedAt = DateTime.Now
+        }).ToArray();
+    }
 }
